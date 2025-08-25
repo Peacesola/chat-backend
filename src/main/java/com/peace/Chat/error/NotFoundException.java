7e9240df2c4feaf01d46047653416269253e4eb3
Peace <pohunmule@gmail.com>
2025-08-25 15:11:41 +0100

    r

--- src/main/java/com/peace/Chat/error/NotFoundException.java
+++ src/main/java/com/peace/Chat/error/NotFoundException.java
@@ -1,5 +1,5 @@
-package com.peace.Chat.error;
+/*package com.peace.Chat.error;
 
 public class NotFoundException extends RuntimeException{
     public NotFoundException(String message) { super(message); }
-}
+}*/
