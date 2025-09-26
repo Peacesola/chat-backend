f14ade54b6ff61b9e85dc667512ebb975c3d6b27
Peace <pohunmule@gmail.com>
2025-09-26 13:59:22 +0100

    changed cors

--- src/main/java/com/peace/Chat/firebase/FirebaseInitializer.java
+++ src/main/java/com/peace/Chat/firebase/FirebaseInitializer.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,8 +16,11 @@ public class FirebaseInitializer {
 
     @PostConstruct
     public void init() throws IOException {
-        InputStream serviceAccount = getClass().getClassLoader()
-                .getResourceAsStream("serviceAccountKey.json");
+        /*InputStream serviceAccount = getClass().getClassLoader()
+                .getResourceAsStream("serviceAccountKey.json");*/
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+
         FirebaseOptions options= FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
